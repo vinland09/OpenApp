75a966ae2bdfc615161ba9d92f236f67a9ea1c0b
Vinicius Landim <vinicius.landim.09@outlook.com>
2025-03-21 11:07:25 -0300

    Commit para OpenApp

--- OpenApps.py
+++ OpenApps.py
@@ -1,13 +1,13 @@
 import subprocess
 
 # Função para abrir um aplicativo
-def abrir_app(canvas):
+def abrir_app(Outlook):
     try:
-        subprocess.Popen(canvas)
-        print(f"Aplicativo {canvas} aberto com sucesso.")
+        subprocess.Popen(Outlook)
+        print(f"Aplicativo {Outlook} aberto com sucesso.")
     except FileNotFoundError:
-        print(f"Aplicativo {canvas} não encontrado.")
+        print(f"Aplicativo {Outlook} não encontrado.")
 
 # Caminho completo do aplicativo que você deseja abrir
-canvas_app = r"C:\Users\Vinicius\AppData\Local\Programs\Canva\Canva.exe"
-abrir_app(canvas_app)
+Outlook_app = r"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+abrir_app(Outlook_app)
